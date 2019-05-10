b7c47098216bb793825e9490fa7b995d8287c8e4
nickyzj <reg.zj@outlook.com>
2019-05-10 12:25:56 +0800

    Fix env switch

--- batch_job/config.py
+++ batch_job/config.py
@@ -4,8 +4,10 @@ from batch_job.switch_env import switch_env
 
 if switch_env == "qcb":
     import rfc_connection.qcb as current_env
+    print("Current env: qcb")
 elif switch_env == "pcb":
     import rfc_connection.pcb as current_env
+    print("Current env: pcb")
 
 user = current_env.user
 ashost = current_env.ashost
